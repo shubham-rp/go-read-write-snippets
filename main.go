8c91782e91a40297801f8c17619dd026369dc36e
Shubham Phadte <shubhamphadte37@gmail.com>
2024-05-05 23:03:58 +0530

    append operation - combination of read and write

--- main.go
+++ main.go
@@ -7,6 +7,25 @@ import (
 )
 
 func main() {
+
+	// 1. Append - Read & write 
+	by,err := os.ReadFile("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(by))
+
+	msg := []byte("\nappended message")
+
+	by = append(by, msg...)
+	
+	err = os.WriteFile("test.txt", by, 0777)
+	if err !=nil {
+		log.Fatal(err)
+	}
+
+	// 2. Write Operation
 	// data := []byte("practicego")
 	// err := os.WriteFile("test.txt", data, 0777)
 	// if err !=nil {
@@ -14,10 +33,11 @@ func main() {
 	// }
 	// fmt.Println("success")
 
-	by,err := os.ReadFile("test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// 3. Read Operation
+	// by,err := os.ReadFile("test.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Println(string(by))
+	// fmt.Println(string(by))
 }
